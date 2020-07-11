70bcefc8e8a3b0b50faadc265ec91741c26e7e5e
anna-chertova <ianet@yandex.ru>
2020-07-11 14:04:53 +0300

    Fix formatting Add some comments

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -18,20 +18,28 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    setWindowTitle("Company viewer - Unigine");
     createActions();
 
-    setCentralWidget(treeView);
+    setWindowTitle("Company viewer - Unigine");
 
-    standardModel->setHorizontalHeaderLabels({"Department name", "Num employees", "Avg salary"});
+    // Adjust standard model
+    standardModel->setHorizontalHeaderLabels({"Department name",
+                                              "Num employees",
+                                              "Avg salary"});
 
-    QList<QStandardItem *> preparedRow = prepareRow("Network department", "1", "35000");
+    QList<QStandardItem *> preparedRow = prepareRow("Network department",
+                                                    "1",
+                                                    "35000");
     QStandardItem *root = standardModel->invisibleRootItem();
     root->appendRow(preparedRow);
 
-    QList<QStandardItem *> secondRow = prepareRow("Anna Chertova", "Senior Sotware Developer", "35000");
+    QList<QStandardItem *> secondRow = prepareRow("Anna Chertova",
+                                                  "Senior Sotware Developer",
+                                                  "35000");
     preparedRow.first()->appendRow(secondRow);
 
+    // Adjust treeView
+    setCentralWidget(treeView);
     treeView->setModel(standardModel);
     treeView->expandAll();
     for(int i = 0; i < standardModel->rowCount(); ++i) {
@@ -41,9 +49,7 @@ MainWindow::MainWindow(QWidget *parent)
 
 MainWindow::~MainWindow()
 {
-    //delete standardModel;
-    //delete treeView;
-    //delete ui;
+    // parent will destroy its children
 }
 
 bool MainWindow::loadFile(const QString &fileName)
@@ -51,7 +57,10 @@ bool MainWindow::loadFile(const QString &fileName)
     QFile file(fileName);
     if (!file.open(QFile::ReadOnly | QFile::Text))
     {
-        QMessageBox::warning(this, "Error opening file", "Could not open file", QMessageBox::Ok);
+        QMessageBox::warning(this,
+                             tr("Error opening file"),
+                             tr("Could not open file"),
+                             QMessageBox::Ok);
         return false;
     }
 
@@ -65,7 +74,9 @@ bool MainWindow::loadFile(const QString &fileName)
         {
 
         }
+        xmlReader.readNext();
     }
+    file.close();
     return true;
 }
 
@@ -80,25 +91,35 @@ QList<QStandardItem *> MainWindow::prepareRow(const QString &first,
 
 void MainWindow::createActions()
 {
+    // Create menu actions
     QMenu *menuFile = menuBar()->addMenu(tr("&File"));
-    QAction *actionOpen = menuFile->addAction(tr("&Open..."), this, &MainWindow::open);
+    QAction *actionOpen = menuFile->addAction(tr("&Open..."),
+                                              this,
+                                              &MainWindow::open);
     actionOpen->setShortcut(QKeySequence::Open);
 
-    QAction *actionSaveAs = menuFile->addAction(tr("&Save as..."), this, &MainWindow::saveAs);
+    QAction *actionSaveAs = menuFile->addAction(tr("&Save as..."),
+                                                this,
+                                                &MainWindow::saveAs);
     actionSaveAs->setEnabled(false);
 
-    QAction *actionClose = menuFile->addAction(tr("&Close"), this, &MainWindow::close);
+    QAction *actionClose = menuFile->addAction(tr("&Close"),
+                                               this,
+                                               &MainWindow::close);
     actionClose->setEnabled(false);
 }
 
-static void initializeXmlFileDialog(QFileDialog &dialog, QFileDialog::AcceptMode acceptMode)
+static void initializeXmlFileDialog(QFileDialog &dialog,
+                                    QFileDialog::AcceptMode acceptMode)
 {
     static bool firstDialog = true;
 
     if(firstDialog) {
         firstDialog = false;
-        const QStringList filesLocations = QStandardPaths::standardLocations(QStandardPaths::DocumentsLocation);
-        dialog.setDirectory(filesLocations.isEmpty() ? QDir::currentPath() : filesLocations.last());
+        const QStringList filesLocations = QStandardPaths::standardLocations(
+                    QStandardPaths::DocumentsLocation);
+        dialog.setDirectory(filesLocations.isEmpty() ?
+                                QDir::currentPath() : filesLocations.last());
     }
 
     QStringList mimeTypeFilters({"application/xml"});
@@ -112,18 +133,20 @@ void MainWindow::open()
 {
     QFileDialog dialog(this, tr("Open File"));
     initializeXmlFileDialog(dialog, QFileDialog::AcceptOpen);
-    while (dialog.exec() == QDialog::Accepted && !loadFile(dialog.selectedFiles().first())) {}
+    while (dialog.exec() == QDialog::Accepted &&
+           !loadFile(dialog.selectedFiles().first())) {}
 }
 
 void MainWindow::saveAs()
 {
     QFileDialog dialog(this, tr("Save File As"));
     initializeXmlFileDialog(dialog, QFileDialog::AcceptSave);
-    while (dialog.exec() == QDialog::Accepted && !loadFile(dialog.selectedFiles().first())) {}
+    while (dialog.exec() == QDialog::Accepted &&
+           !loadFile(dialog.selectedFiles().first())) {}
 }
 
 void MainWindow::close()
 {
-
+    /// TODO: clear ui connected with file contents here
 }
 
